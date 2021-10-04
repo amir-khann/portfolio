9c491ab59e418d1746b6d1c1c07f6654681ee2cc
amir khan <amiraup17@gmail.com>
2021-10-04 03:57:26 -0700

    add functionality to links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Common/Button'
 import Card from './Common/Card'
-import abad from '../assets/images/abad.png'
-import shot from '../assets/images/shot_25_artlab_minisite.png'
-import apri from '../assets/images/apri.png'
-
+import img1 from '../assets/images/abad.png'
+import img2 from '../assets/images/shot_25_artlab_minisite.png'
+import img3 from '../assets/images/apri.png'
+import img4 from '../assets/images/nalie.png'
+import img5 from '../assets/images/v1.png'
+import img6 from '../assets/images/bane.png'
 
 const Portfolio = () => {
+
+    const [show, setShow] = useState('all')
+    let all = [img1,img2,img3,img4,img5,img6]
+    let logo = [img3,img4,img5,img6]
+    let website = [img5]
+    let app = [img1,img2,img3,img4,img5,img6]
+
 
     return (
         <div className="portfolio">
@@ -20,10 +29,10 @@ const Portfolio = () => {
                             </div>
                             <div className="links">
                                 <ul>
-                                    <li><a>all</a></li>
-                                    <li><a>Logo</a></li>
-                                    <li><a>Websites</a></li>
-                                    <li><a>Mobile Apps</a></li>
+                                    <li><a onClick={()=>{setShow("all")}} className={show ==="all" ? "active":""}>all</a></li>
+                                    <li><a onClick={()=>{setShow("logo")}} className={show ==="logo" ? "active":""}>Logo</a></li>
+                                    <li><a onClick={()=>{setShow("website")}} className={show ==="website" ? "active":""}>Websites</a></li>
+                                    <li><a onClick={()=>{setShow("app")}} className={show ==="app" ? "active":""}>Mobile Apps</a></li>
                                 </ul>
                                 
                             </div>
@@ -34,12 +43,19 @@ const Portfolio = () => {
                         </div>
                     </div>
                     <div className="cards">
-                        <Card  image={abad}/>
-                        <Card image={shot}/>
-                        <Card image={apri}/>
-                        <Card  image={abad}/>
-                        <Card image={shot}/>
-                        <Card image={apri}/>
+                      { show === "all" && all.map((item) =>(
+                          <Card  image={item}/>
+                           ))}
+
+                    { show === "logo" && logo.map((item) =>(
+                          <Card  image={item}/>
+                           ))}
+                    { show === "website" && website.map((item) =>(
+                    <Card  image={item}/>
+                    ))}
+                    { show === "app" && app.map((item) =>(
+                    <Card  image={item}/>
+                    ))}
                     </div>
                    
                 </div>
